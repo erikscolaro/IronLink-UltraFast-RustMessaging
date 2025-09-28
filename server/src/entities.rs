74f940bff8f9057fabfa29a713ce07c35fb02680
Erik Scolaro <erik.scolaro.dev@gmail.com>
2025-09-28 16:17:01 +0200

    fix: make internal fields public

--- server/src/entities.rs
+++ server/src/entities.rs
@@ -57,46 +57,46 @@ impl User {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Message {
-    id: IdType,
-    chat_id: IdType,
-    sender_id: IdType,
-    content: String,
+    pub id: IdType,
+    pub chat_id: IdType,
+    pub sender_id: IdType,
+    pub content: String,
     // il server si aspetta una stringa litterale iso8601 che viene parsata in oggetto DateTime di tipo UTC
     // la conversione viene fatta in automatico da serde, la feature è stata abilitata
-    created_at: DateTime<Utc>,
+    pub created_at: DateTime<Utc>,
     // campo rinominato rispetto a uml perchè type è una parola protetta
-    message_type: MessageType,
+    pub message_type: MessageType,
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserChatMetadata {
-    user_id: IdType,
-    chat_id: IdType,
-    user_role: UserRole,
+    pub user_id: IdType,
+    pub chat_id: IdType,
+    pub user_role: UserRole,
     // sostituisce deliver_from con un nome più esplicativo
     // sostituito al posto dell'id del messaggio il datetime, è da intendersi come
     // "visualizza i messaggi da questo istante in poi, questo istante ESCLUSO"
-    messages_visible_from: DateTime<Utc>,
+    pub messages_visible_from: DateTime<Utc>,
     // sostituisce last delivered con un nume più esplicativo
     // sostituito al posto dell'id del messaggio il date time, è da intendersi come
     // "ho ricevuto i messaggi fino a questo istante, istante INCLUSO"
-    messages_received_until: DateTime<Utc>,
+    pub messages_received_until: DateTime<Utc>,
     //per ora non esludo i due campi dalla deserializzazione
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Invitation {
-    id: IdType,
+    pub id: IdType,
     // rinominato da group_id a chat_id per consistenza
-    chat_id: IdType,    // chat ( di gruppo ) in cui si viene invitati
-    invited_id: IdType, // utente invitato
-    invitee_id: IdType, // utente che invita
-    state: InvitationStatus,
+    pub chat_id: IdType,    // chat ( di gruppo ) in cui si viene invitati
+    pub invited_id: IdType, // utente invitato
+    pub invitee_id: IdType, // utente che invita
+    pub state: InvitationStatus,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Chat {
-    id: IdType,
-    title: Option<String>,
-    description: Option<String>,
-    chat_type: ChatType,
+    pub id: IdType,
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub chat_type: ChatType,
 }
