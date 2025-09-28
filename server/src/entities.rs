07f2dcda9d2a7831dfce25d173a31fb040cb33bd
Erik Scolaro <erik.scolaro.dev@gmail.com>
2025-09-28 15:43:05 +0200

    feat: split models into entities.rs and dtos.rs for clarity

--- server/src/entities.rs
+++ server/src/entities.rs
@@ -55,25 +55,6 @@ impl User {
     }
 }
 
-// struct per gestire io col client
-#[derive(Serialize, Deserialize, Debug)]
-pub struct UserDTO {
-    pub id: Option<IdType>,
-    pub username: Option<String>,
-    #[serde(skip_serializing)]
-    pub password: Option<String>,
-}
-
-impl From<User> for UserDTO {
-    fn from(value: User) -> Self {
-        Self {
-            id: Some(value.id),
-            username: Some(value.username),
-            password: None, // mai esposta al client!!!
-        }
-    }
-}
-
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Message {
     id: IdType,
