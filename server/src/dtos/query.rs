703721a989e7b4ace2b6c7d120c865292fbfdbe3
Erik Scolaro <erik.scolaro.dev@gmail.com>
2025-10-17 21:56:06 +0200

    refactor(dtos): rename SearchQueryDTO to UserSearchQuery and add MessagesQuery

--- server/src/dtos/query.rs
+++ server/src/dtos/query.rs
@@ -1,9 +1,17 @@
 //! Query DTOs - Data Transfer Objects per query di ricerca
 
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-/// DTO per ricerche con query string
+/// DTO per query parameters di ricerca utenti
 #[derive(Serialize, Deserialize, Debug)]
-pub struct SearchQueryDTO {
+pub struct UserSearchQuery {
     pub search: String,
+}
+
+/// DTO per query parameters di paginazione messaggi
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MessagesQuery {
+    #[serde(default)]
+    pub before_date: Option<DateTime<Utc>>,
 }
